95e09d3ca86626aaaafed84c3d38b25038c4af3d
AbrahamRiRa <abrahmr0@gmail.com>
2022-09-30 20:49:58 -0500

    commit inicial

---
+++ jquery/ej1.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#div1").hide(1000);
+    });
+
+    $("#btn2").click(function(){
+        $("#div1").show(1000);
+    });
+});
